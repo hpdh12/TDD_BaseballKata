77adf49803818a7237dc538a8775d0b536c721e3
Dongseong <hpdh12@me.com>
2023-07-06 08:40:16 +0900

    [refactor] p42

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -6,24 +6,33 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const std::string& guessNumber)
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1] ||
+			guessNumber[0] == guessNumber[2] ||
+			guessNumber[1] == guessNumber[2];
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
 	{
 		if (guessNumber.length() != 3)
 			throw length_error("Must be tree letters");
 
 		for (char ch : guessNumber)
 		{
-			if (ch < '0' || ch > '9')
-			{
-				throw invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9')
+				continue;
+			throw invalid_argument("Must be number");
 		}
 
-		if (guessNumber[0] == guessNumber[1] ||
-			guessNumber[0] == guessNumber[2] ||
-			guessNumber[1] == guessNumber[2])
+		if (isDuplicatedNumber(guessNumber))
 		{
 			throw invalid_argument("Must not have the same number");
 		}
+	}
+
+	void guess(const std::string& guessNumber)
+	{
+		assertIllegalArgument(guessNumber);
 	}
 };
